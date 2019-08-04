f6a23353c003cb85b02bff501a75cef18a76c97b
Jordan Matelsky <j6k4m8@gmail.com>
2019-08-04 18:39:01 -0400

    Add inline documentation

--- prefect/__init__.py
+++ prefect/__init__.py
@@ -1,16 +1,64 @@
+"""
+Main file for prefect Python library for interacting with the MyFordMobile API
+servers. These servers host a SOAP-style API that sometimes bhas a few REST-API
+flavored artifacts.
+"""
+
 from typing import List
 import requests
 
 
 class FordAPI:
-    def __init__(self):
+    """
+    High-level class for interacting with the MyFordMobile API.
+
+    Authentication is handled separately so that a FordAPI object can be
+    instantiated without having to deal with authentication at creationtime.
+    (In this way, we can more easily pickle and pass around objects without
+    worrying about security leak.)
+    """
+
+    def __init__(self) -> None:
+        """
+        Create a new FordAPI object.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+
+        """
         self._base_url = "https://www.myfordmobile.com"
         self._token = None
 
     def url(self, suffix: str = "/") -> str:
+        """
+        Construct a URL based upon the _base_url of this object.
+
+        This method will probably only be called privately.
+
+        Arguments:
+            suffix (str): The URL endpoint to concat to the _base_url
+
+        Returns:
+            str: A fully-qualified URL
+
+        """
         return f"{self._base_url}/{suffix}"
 
     def post(self, url: str, data: dict) -> requests.Response:
+        """
+        Send a POST HTTP request to a given URL with a payload.
+
+        Arguments:
+            url (str): The URL endpoint to access
+            data (dict): A JSONifiable dictionary to send as the request.body
+
+        Returns:
+            requests.Response: The response from the HTTP request
+
+        """
         return requests.post(
             url,
             headers={
@@ -21,6 +69,21 @@ class FordAPI:
         )
 
     def authenticate(self, username: str, password: str) -> None:
+        """
+        Perform a server authentication.
+
+        TODO: This should be returning a token
+        TODO: Should also accept zero arguments, in which case a local config
+              file (~/.config/myfordmobile.json?) holds the credentials
+
+        Arguments:
+            username (str): User's email
+            password (str): The user's password
+
+        Returns:
+            None
+
+        """
         params = {
             "PARAMS": {
                 "emailaddress": username,
@@ -33,9 +96,22 @@ class FordAPI:
 
         # TODO: We should get back a token in here???
         # Set this._token.
+        # TODO: Returning for debug only.
         return res
 
     def get_vehicles(self) -> List[dict]:
+        """
+        Get a list of all registered vehicles for this account.
+
+        TODO: Return List[Vehicle] instead of List[dict]
+
+        Arguments:
+            None
+
+        Returns:
+            List[dict]: A list of vehicles, in dict metadata form
+
+        """
         params = {
             "PARAMS": {
                 "SESSIONID": self._token,
